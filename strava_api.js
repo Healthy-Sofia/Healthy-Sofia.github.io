f93ee904811e1ddb4c2b89d6575d8e6bcfd87c7d
zachdimitrov <zach_dimitrov@yahoo.com>
2021-06-03 03:33:04 +0300

    change env

--- strava_api.js
+++ strava_api.js
@@ -55,9 +55,9 @@ function reAuthorize() {
             'Content-Type': 'application/json'
         },
         body: JSON.stringify({
-            client_id: env.CLIENT_ID,
-            client_secret: env.CLIENT_SECRET,
-            refresh_token: env.REFRESH_TOKEN,
+            client_id: process.env.CLIENT_ID,
+            client_secret: process.env.CLIENT_SECRET,
+            refresh_token: process.env.REFRESH_TOKEN,
             grant_type: 'refresh_token'
         })
     }).then((res) => res.json())
